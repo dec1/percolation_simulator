b4cad7968919f517dbb1bcae4bcbc9d4c97496b2
dm <5962877+dec1@users.noreply.github.com>
2024-06-20 19:57:32 +0200

    fix venv create script on windows

--- prj/script/venv_create.cmd
+++ prj/script/venv_create.cmd
@@ -12,26 +12,27 @@ REM    - that set in `common.scmd`  if any, or if not set there then
 REM    - whatever is found as "python" on path
 REM -----------------------------------------------------------
 if "%1"=="" (
+    echo "no python version (arg) passed to venv_create.cmd"
     if "%py_ver%"=="" (
+        echo "no python version defined in common.cmd"
         REM Default to system Python if no version is specified
         set py_command=python
+        echo " .... using versionless
     ) else (
         set py_command=py -%py_ver%
+        echo "using py_ver from common.cmd" %py_ver%
     )
 ) else (
     set py_command=py -%1
 )
-
+echo "using py_command:" %py_command%
 echo Using Python version: %py_ver%
 
 REM -----------------------------------------------------------
 REM -----------------------------------------------------------
 
 REM Create the virtual environment using the py launcher
-CALL py -%py_ver% -m venv "%venv_dir%
-
-
-CALL python -m venv %venv_dir%
+CALL %py_command% -m venv "%venv_dir%
 
 echo ".....virtual environment created"
 
